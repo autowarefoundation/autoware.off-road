c1ca1328abcdd9eb4cb13dc24ffbb6c1c9f841be
Karthikeya Jayaram <karthikeyaj82@gmail.com>
2025-06-27 17:26:38 -0400

    Refactor GooseDataset to include Albumentations for data augmentation and update transformation logic for training and validation splits.

--- ObjectSeg/dataset-process/goose_dataset.py
+++ ObjectSeg/dataset-process/goose_dataset.py
@@ -9,7 +9,9 @@ import argparse
 from tqdm import tqdm
 from PIL import Image
 import torchvision.transforms as T
-from utils import get_mean_std
+from utils import get_mean_std, build_segformer_train_augs
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
 class GooseDataset(Dataset):
     def __init__(self, root_dir, split="train", transform=None):
@@ -17,7 +19,11 @@ class GooseDataset(Dataset):
         mask_pattern = os.path.join(root_dir, "labels", split, "**", "*_color.png")
         self.img_paths = sorted(glob(img_pattern, recursive=True))
         self.mask_paths = sorted(glob(mask_pattern, recursive=True))
-        self.transform = transform
+        self.transform = build_segformer_train_augs() if split == "train" else A.Compose([
+            A.Resize(height=512, width=512),
+            A.Normalize(mean=(123.675,116.28,103.53), std=(58.395,57.12,57.375), max_pixel_value=255.0),
+            ToTensorV2()
+        ])
 
     def __len__(self):
         return len(self.img_paths)
@@ -30,7 +36,8 @@ class GooseDataset(Dataset):
         mask = cv2.imread(mask_fp, cv2.IMREAD_GRAYSCALE).astype(np.int64)
         if self.transform:
             img = self.transform(img)
-        return {"image": torch.from_numpy(img).permute(2, 0, 1), "mask": torch.from_numpy(mask)}
+        return {"image": torch.from_numpy(img).permute(2, 0, 1) # (C,H,W)
+                , "mask": torch.from_numpy(mask).long()}
 
 
 if __name__ == "__main__":
@@ -51,31 +58,31 @@ if __name__ == "__main__":
     dataset = GooseDataset(root_dir=args.root_dir, split=args.split)
     print(f"Dataset size: {len(dataset)}")
 
-    mean, std = get_mean_std(dataset)
+    #mean, std = get_mean_std(dataset)
 
-    print(f"mean: {mean}")
-    print(f"std: {std}")
+    #print(f"mean: {mean}")
+    #print(f"std: {std}")
 
-    img, mask = dataset[args.index]["image"], dataset[args.index]["mask"]
-    img = img.permute(1, 2, 0).numpy()
-    mask = mask.numpy()
+    # img, mask = dataset[args.index]["image"], dataset[args.index]["mask"]
+    # img = img.permute(1, 2, 0).numpy()
+    # mask = mask.numpy()
     
-    # Create 1x2 subplot
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 7))
+    # # Create 1x2 subplot
+    # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 7))
     
-    # Plot original image
-    ax1.imshow(img)
-    ax1.set_title('Original Image')
-    ax1.axis('off')
+    # # Plot original image
+    # ax1.imshow(img)
+    # ax1.set_title('Original Image')
+    # ax1.axis('off')
     
-    # Plot mask
-    ax2.imshow(mask)
-    ax2.set_title('Mask')
-    ax2.axis('off')
+    # # Plot mask
+    # ax2.imshow(mask)
+    # ax2.set_title('Mask')
+    # ax2.axis('off')
     
-    plt.tight_layout()
-    plt.savefig(args.output, dpi=150, bbox_inches='tight')
-    plt.close()
-    print(f"Visualization saved as '{args.output}'")
+    # plt.tight_layout()
+    # plt.savefig(args.output, dpi=150, bbox_inches='tight')
+    # plt.close()
+    # print(f"Visualization saved as '{args.output}'")
 
 
