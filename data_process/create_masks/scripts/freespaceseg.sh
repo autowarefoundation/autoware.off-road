b5694278ae468106e333b9c63415e1d624d1f0ba
pojenwang <pw.pjwang@gmail.com>
2025-08-20 13:05:32 -0700

    scriptes to build freespaceseg, objectseg, terrainseg datasets

---
+++ data_process/create_masks/scripts/freespaceseg.sh
@@ -0,0 +1,6 @@
+python create_mask.py label_config=freespaceseg_rellis3d   color_map=freespaceseg
+python create_mask.py label_config=freespaceseg_goose      color_map=freespaceseg
+python create_mask.py label_config=freespaceseg_yamahacmu  color_map=freespaceseg
+python create_mask.py label_config=freespaceseg_orfd       color_map=freespaceseg
+python create_mask.py label_config=freespaceseg_cassed     color_map=freespaceseg
+python create_mask.py label_config=freespaceseg_offsed     color_map=freespaceseg
---
+++ data_process/create_masks/scripts/objectseg.sh
@@ -0,0 +1,3 @@
+python create_mask.py label_config=objectseg_rellis3d   color_map=objectseg
+python create_mask.py label_config=objectseg_goose      color_map=objectseg
+python create_mask.py label_config=objectseg_offsed     color_map=objectseg
